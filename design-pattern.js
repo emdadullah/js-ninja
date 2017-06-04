bbc249cffed67d1afda2f45a7dd905e833f4fe6f
emdadullah <kaziemdadullah@yahoo.com>
2017-06-04 12:01:46 +0600

    Module pattern is added

--- design-pattern.js
+++ design-pattern.js
@@ -4,3 +4,18 @@
 (function($){
 	
 })(jQuery)
+
+
+
+/* 2. Module pattern */
+
+var M = M || {};
+
+M = (function(){
+		
+	var publicFunc = function function_name (argument) {
+		// body...
+	};
+
+	return publicFunc;
+})();
